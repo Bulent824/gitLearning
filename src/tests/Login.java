fe888bfefef67b1a1aaf3737c23ac83f96e1d495
Bulent824 <bulentozturk2019@outlook.com>
2020-05-05 18:55:36 +0200

    add Login.java

---
+++ src/tests/Login.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class Login {
+
+    public static void main(String[] args) {
+        
+        String name = "Bulent";
+        System.out.println("name = " + name);
+    }
+}
