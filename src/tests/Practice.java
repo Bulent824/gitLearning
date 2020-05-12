5970b03155b82b869668ec16d55ff0e72a5092cb
Bulent824 <62132667+Bulent824@users.noreply.github.com>
2020-05-12 17:06:42 +0200

    Update Practice.java

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -5,5 +5,9 @@ public class Practice {
     public static void main(String[] args) {
 
         System.out.println("Merhaba");
+        
+        
+        
+        System.out.println("Merhaba" + "nasilsin");
     }
 }
