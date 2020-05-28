df018c878b9b16bb9515d50c067b5aa28218ec01
Bulent824 <62132667+Bulent824@users.noreply.github.com>
2020-05-29 01:39:30 +0200

    Update Deneme.java

--- src/tests/Deneme.java
+++ src/tests/Deneme.java
@@ -4,5 +4,6 @@ public class Deneme {
 
     public static void main(String[] args) {
         System.out.println("hava guzel");
+        System.out.println("hava guzel gercekten cok mu guzel");
     }
 }
